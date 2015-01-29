ec6fcbdb1e3df1769be8ece3552e35fd5c1e3e46
Matthew Boyd <boyd0077@algonquincollege.com>
2015-01-29 14:13:09 -0500

    Add comments to code

--- MyElective/src/com/myelective/jbdc/DBUtility.java
+++ MyElective/src/com/myelective/jbdc/DBUtility.java
@@ -3,10 +3,24 @@ package com.myelective.jbdc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+/**
+* A class used to manage the Connection to the SQL Database
+*
+* @author Matthew Boyd
+* @version 0.1 
+*/
 public class DBUtility {
 	
 	private static Connection connection = null;
 	
+	/**
+	 * Returns a Connection to the SQL database. If a Connection is already
+	 * established, returns the current Connection. If a Connection
+	 * is not established, connects to the database and returns the created
+	 * Connection
+	 * 
+	 * @return 		the Connection to the database
+	 */
 	public static Connection getConnection() {
 		
 		if (connection != null){
